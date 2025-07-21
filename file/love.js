d3695df16debc8a4a8cc1fbfe65c3657b8c086e6
Prasunet <162030269+z9xqv7mkt3wl8psa@users.noreply.github.com>
2025-07-22 00:14:29 +0530

    Update love.js

--- file/love.js
+++ file/love.js
@@ -169,7 +169,7 @@
             ctx.scale(0.75, 0.75);
             ctx.font = "12px,Verdana"; // 字号肿么没有用? (ˉ(∞)ˉ)
             ctx.fillText("Click Me:) ", 30, -5);
-            ctx.fillText("Birthday Queen !", 28, 10);
+            ctx.fillText("My Dear Love !", 28, 10);
             ctx.restore();
         },
         clear: function() {
